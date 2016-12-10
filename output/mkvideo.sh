51622f2abf036f1fe8d0a93bd058815bdaec557c
Julian Fuchs <jfuchs@mit.edu>
2016-12-10 17:42:42 -0500

    Allow variable FPS in mkvideo script

--- output/mkvideo.sh
+++ output/mkvideo.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 set -e
-ffmpeg -hide_banner -loglevel error -i frame-%04d.png ../output.mp4
+
+if [ -z "$1" ]; then
+    FPS=25
+else
+    FPS=$1
+fi
+
+ffmpeg -hide_banner -loglevel error -r $FPS -i frame-%04d.png ../output.mp4
 echo Created ../output.mp4
